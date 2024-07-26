c5a9aec10b850b137245dfa4ab97171dbb4b54a5
starkey <tanishqgupta013@gmail.com>
2024-07-26 20:36:03 +0530

    mongo db connected

--- index.js
+++ index.js
@@ -2,8 +2,38 @@ const express = require('express');
 const fs = require('fs');
 const users = require('./MOCK_DATA.json')
 const app = express();
-const users = require('mongoose');
+const mongoose = require('mongoose');
+const { timeStamp } = require('console');
 const PORT = 9000;
+
+//connection
+mongoose
+.connect("mongodb://127.0.0.1:27017/nodejs")
+.then(() => console.log("mongo db Connected"))
+.catch((err) => console.log("Mongo ERROR" , err));
+
+//schema
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+    },
+    lastName: {
+        type:String,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    gender:{
+        type:String,
+    },
+    
+},{timestamps:true})
+
+
+ const user = mongoose.model("user" , userSchema);
+ 
 //middlewares
 app.use(express.urlencoded({extended: false}));
 app.use((req,res,next) => {
@@ -19,11 +49,15 @@ app.use((req,res,next) => {
 
 //Routes
 
+// for delete findByIdAndDelete( req.params.id , { jo bhi delete krna hai wo})
+// for patch findByIdAndDelete( req.params.id , { jo bhi change krna hai wo})
+
 app.route('/api/users/:id')
-.get( (req, res) => {
+.get( async(req, res) => {
+    const USER = await user.findById(req.params.id);
     const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user)
+    
+    return res.json(USER)
 })
 .patch( (req, res) => {
     return res.json({ status: "pending"})
@@ -33,16 +67,33 @@ app.route('/api/users/:id')
 })
 
 
-app.get('/api/users', (req, res) => {
-    return res.json(users)
+app.get('/api/users', async(req, res) => {
+    const dbUsers = await user.find({});
+    const dbfirstname = dbUsers.map((user) => user.firstName)
+    res.json(dbfirstname);
 })
 
-app.post('/api/users', (req, res) => {
+app.post('/api/users', async(req, res) => {
     const body = req.body;
-    users.push({ id: users.length +1,...body});
-    fs.writeFile('./MOCK_DATA.json' , JSON.stringify(users) , (err, data) => {
-        return res.json({status: 'pending'});
-    }) 
+    if(
+        !body ||
+        !body.first_name ||
+        !body.last_name ||
+        !body.email ||
+        !body.gender ||
+        !body.ip 
+    )
+    {return resizeTo.status(400).json({msg: "All fields are req..."})}
+    const result = await user.create({
+        firstName: body.first_name,
+        lastName: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        ip: body.ip
+    });
+    console.log(result)
+
+    return res.status(201).json({ msg: 'success'});
 })
 
 
